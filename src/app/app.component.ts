626245f871b5f111d30f8114067dfc4f2fecae3f
Dale Nguyen <dungnq@itbox4vn.com>
2023-12-24 23:16:26 -0500

    add sidebar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,24 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { SidebarComponent } from './libs/shell/sidebar/sidebar/sidebar.component';
 import { FooterComponent } from './libs/shell/footer/footer.component';
+import { StyleClassModule } from 'primeng/styleclass';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   template: `
     <div class="flex h-full">
-      <app-sidebar />
+      <div class="flex flex-row-reverse">
+        <a
+          class="cursor-pointer block lg:hidden"
+          pStyleClass="@next"
+          enterFromClass="hidden"
+          leaveToClass="hidden"
+        >
+          <i class="pi pi-bars text-4xl"></i>
+        </a>
+        <app-sidebar class=" lg:block" />
+      </div>
       <div>
         <main>
           <router-outlet />
@@ -25,6 +36,6 @@ import { FooterComponent } from './libs/shell/footer/footer.component';
       }
     `,
   ],
-  imports: [RouterOutlet, SidebarComponent, FooterComponent],
+  imports: [RouterOutlet, StyleClassModule, SidebarComponent, FooterComponent],
 })
 export class AppComponent {}
--- src/app/libs/shell/sidebar/sidebar/sidebar.component.ts
+++ src/app/libs/shell/sidebar/sidebar/sidebar.component.ts
@@ -1,28 +1,195 @@
-import { SidebarModule } from 'primeng/sidebar';
 import { ButtonModule } from 'primeng/button';
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { AvatarModule } from 'primeng/avatar';
+import { Router } from '@angular/router';
+import { DividerModule } from 'primeng/divider';
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [CommonModule, ButtonModule, SidebarModule],
+  imports: [CommonModule, ButtonModule, AvatarModule, DividerModule],
   template: `
-    <div class="flex justify-content-center">
-      SIDEBAR
+    <div class="bg-black-alpha-80 p-4 text-white h-full">
+      <!-- Header -->
+      <div id="header">
+        <div class="top mb-8 text-right">
+          <!-- Logo -->
+          <div id="logo" class="flex justify-content-between">
+            <p-avatar
+              image="assets/images/dale-nguyen-avatar.jpeg"
+              size="xlarge"
+              shape="square"
+            />
+            <div>
+              <h1 id="title" class="m-0">Dale Nguyen</h1>
+              <p>Software Engineer</p>
+              <p>Toronto Canada</p>
+            </div>
+          </div>
+        </div>
 
-      <p-button
-        (click)="sidebarVisible = true"
-        icon="pi pi-arrow-right"
-      ></p-button>
+        <div class="mb-8">
+          <!-- Nav -->
+          <nav id="nav">
+            <ul class="list-none p-0">
+              <li
+                class="p-2 mb-6 font-semibold text-xl cursor-pointer"
+                [ngClass]="isActive('intro')"
+                (click)="scroll('intro')"
+              >
+                <div id="top-link" class="flex align-content-center">
+                  <i class="pi pi-home mr-4" style="font-size: 2rem"></i>
+
+                  Architectural Philosophy
+                </div>
+              </li>
+              <li
+                class="p-2 mb-6 font-semibold text-xl cursor-pointer"
+                [ngClass]="isActive('blog')"
+                (click)="navigateTo('blog')"
+              >
+                <div
+                  id="blog-link"
+                  class="flex align-items-center justify-content-between"
+                >
+                  <i class="pi pi-file-edit" style="font-size: 2rem"></i>
+
+                  Thoughts
+                </div>
+              </li>
+              <!-- <li [ngClass]="isActive('portfolio')" (click)="scroll('portfolio')">
+          <div id="portfolio-link" class="nav flex justify-content-between">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              class="w-2 h-2"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M3.375 19.5h17.25m-17.25 0a1.125 1.125 0 01-1.125-1.125M3.375 19.5h7.5c.621 0 1.125-.504 1.125-1.125m-9.75 0V5.625m0 12.75v-1.5c0-.621.504-1.125 1.125-1.125m18.375 2.625V5.625m0 12.75c0 .621-.504 1.125-1.125 1.125m1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125m0 3.75h-7.5A1.125 1.125 0 0112 18.375m9.75-12.75c0-.621-.504-1.125-1.125-1.125H3.375c-.621 0-1.125.504-1.125 1.125m19.5 0v1.5c0 .621-.504 1.125-1.125 1.125M2.25 5.625v1.5c0 .621.504 1.125 1.125 1.125m0 0h17.25m-17.25 0h7.5c.621 0 1.125.504 1.125 1.125M3.375 8.25c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125m17.25-3.75h-7.5c-.621 0-1.125.504-1.125 1.125m8.625-1.125c.621 0 1.125.504 1.125 1.125v1.5c0 .621-.504 1.125-1.125 1.125m-17.25 0h7.5m-7.5 0c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125M12 10.875v-1.5m0 1.5c0 .621-.504 1.125-1.125 1.125M12 10.875c0 .621.504 1.125 1.125 1.125m-2.25 0c.621 0 1.125.504 1.125 1.125M13.125 12h7.5m-7.5 0c-.621 0-1.125.504-1.125 1.125M20.625 12c.621 0 1.125.504 1.125 1.125v1.5c0 .621-.504 1.125-1.125 1.125m-17.25 0h7.5M12 14.625v-1.5m0 1.5c0 .621-.504 1.125-1.125 1.125M12 14.625c0 .621.504 1.125 1.125 1.125m-2.25 0c.621 0 1.125.504 1.125 1.125m0 1.5v-1.5m0 0c0-.621.504-1.125 1.125-1.125m0 0h7.5"
+              />
+            </svg>
+
+            Digital Portfolio
+          </div>
+        </li> -->
+              <li
+                class="p-2 mb-6 font-semibold text-xl cursor-pointer"
+                [ngClass]="isActive('about')"
+                (click)="scroll('about')"
+              >
+                <div
+                  id="about-link"
+                  class="flex align-items-center justify-content-between"
+                >
+                  <i class="pi pi-user" style="font-size: 2rem"></i>
+
+                  Biography
+                </div>
+              </li>
+              <li
+                class="p-2 mb-6 font-semibold text-xl cursor-pointer"
+                [ngClass]="isActive('contact')"
+                (click)="scroll('contact')"
+              >
+                <div
+                  id="contact-link"
+                  class="flex align-items-center justify-content-between"
+                >
+                  <i class="pi pi-envelope" style="font-size: 2rem"></i>
+
+                  Contact
+                </div>
+              </li>
+            </ul>
+          </nav>
+        </div>
+
+        <p-divider />
+
+        <!-- Social Icons -->
+        <div class="mt-8">
+          <ul class="list-none p-0 flex justify-content-around">
+            <li>
+              <a
+                href="https://www.linkedin.com/in/dalenguyenblogger/"
+                class="icon"
+                target="_blank"
+              >
+                <i
+                  class="pi pi-linkedin"
+                  style="font-size: 2rem; color: white"
+                ></i>
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://github.com/dalenguyen"
+                class="icon"
+                target="_blank"
+              >
+                <i
+                  class="pi pi-github"
+                  style="font-size: 2rem; color: white"
+                ></i>
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://twitter.com/dale_nguyen"
+                class="icon"
+                target="_blank"
+              >
+                <i
+                  class="pi pi-twitter"
+                  style="font-size: 2rem; color: white"
+                ></i>
+              </a>
+            </li>
+            <li>
+              <a href="mailto:dale@dalenguyen.me" class="icon">
+                <i
+                  class="pi pi-envelope"
+                  style="font-size: 2rem; color: white"
+                ></i>
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
     </div>
   `,
-  styles: ``,
 })
 export class SidebarComponent {
-  sidebarVisible: boolean = true;
+  private router = inject(Router);
+  // private navService = inject(NavService)
 
-  test() {
-    console.log('????');
+  activeEl = 'intro';
+
+  scroll(id: string) {
+    // TODO: figured out how to navigate on mobile
+    this.activeEl = id;
+    if (this.router.url !== '/') {
+      this.router.navigate(['']);
+      setTimeout(() => {
+        // this.navService.scroll(id)
+        // this.navService.target.next(null)
+      }, 1000);
+    } else {
+      // this.navService.scroll(id)
+      // this.navService.target.next(null)
+    }
+  }
+
+  isActive(id: string) {
+    if (id === this.activeEl) {
+      return 'active';
+    }
+    return '';
   }
 }

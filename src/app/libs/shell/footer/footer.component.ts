ff79833896311a7f98588acdd94eebd93a321dcb
Dale Nguyen <dungnq@itbox4vn.com>
2023-12-26 22:38:37 -0500

    add footer

--- src/app/libs/shell/footer/footer.component.ts
+++ src/app/libs/shell/footer/footer.component.ts
@@ -1,17 +1,23 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { CommonModule } from '@angular/common'
+import { Component, VERSION } from '@angular/core'
 
 @Component({
   selector: 'app-footer',
   standalone: true,
   imports: [CommonModule],
   template: `
-    <p>
-      footer works!
-    </p>
+    <footer class="mx-auto w-full px-4 sm:px-6 lg:px-8">
+      <div class="border-t border-slate-900/5 py-4">
+        <p class="mt-5 text-center text-sm leading-6 text-slate-500">
+          Dale Nguyen © {{ currentYear }} - By using Angular
+          {{ angularVersion }} &
+          <a href="https://analogjs.org/" target="_blank">Analogjs</a>
+        </p>
+      </div>
+    </footer>
   `,
-  styles: ``
 })
 export class FooterComponent {
-
+  currentYear = new Date().getFullYear()
+  angularVersion = VERSION.major
 }

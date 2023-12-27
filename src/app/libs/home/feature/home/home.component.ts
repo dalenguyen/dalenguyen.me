feab8d911b4df93b601e4130a1253f3316c1b111
Dale Nguyen <dungnq@itbox4vn.com>
2023-12-26 22:04:12 -0500

    add biography

--- src/app/libs/home/feature/home/home.component.ts
+++ src/app/libs/home/feature/home/home.component.ts
@@ -1,12 +1,15 @@
-import { CommonModule } from "@angular/common";
-import { Component } from "@angular/core";
-import { IntroComponent } from "../../ui/intro/intro.component";
+import { CommonModule } from '@angular/common'
+import { Component } from '@angular/core'
+import { BiographyComponent } from '../../ui/biography/biography.component'
+import { IntroComponent } from '../../ui/intro/intro.component'
 
 @Component({
-  selector: "app-home",
+  selector: 'app-home',
   standalone: true,
-  template: ` <app-intro /> `,
-  styles: ``,
-  imports: [CommonModule, IntroComponent],
+  template: `
+    <app-intro />
+    <app-biography />
+  `,
+  imports: [CommonModule, IntroComponent, BiographyComponent],
 })
 export class HomeComponent {}

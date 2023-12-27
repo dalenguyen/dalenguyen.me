a4ad2ff3864db68eab04baa873401612f31f2aa0
Dale Nguyen <dungnq@itbox4vn.com>
2023-12-26 22:35:50 -0500

    add contact

--- src/app/libs/home/feature/home/home.component.ts
+++ src/app/libs/home/feature/home/home.component.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common'
 import { Component } from '@angular/core'
 import { BiographyComponent } from '../../ui/biography/biography.component'
+import { ContactComponent } from '../../ui/contact/contact.component'
 import { IntroComponent } from '../../ui/intro/intro.component'
 
 @Component({
@@ -9,7 +10,8 @@ import { IntroComponent } from '../../ui/intro/intro.component'
   template: `
     <app-intro />
     <app-biography />
+    <app-contact />
   `,
-  imports: [CommonModule, IntroComponent, BiographyComponent],
+  imports: [CommonModule, IntroComponent, BiographyComponent, ContactComponent],
 })
 export class HomeComponent {}
